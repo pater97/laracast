8648fe62b646375a9de003967b335ccefc704355
pater97 <giuseppepater997@gmail.com>
2022-01-07 18:06:09 +0100

    header completato

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,47 @@
 <header>
-    
+    <section class="container-fluid">
+        <!-- logo laracast -->
+        <div class="left_header">
+            <h3>
+                LARACAST
+            </h3>
+        </div>
+        <!-- scelte multiple menu -->
+        <div class="center_header d-flex">
+            <div class="options">
+                <a href="#">
+                    TOPICS
+                </a>
+            </div>
+            <div class="options">
+                <a href="#">
+                    SERIES
+                </a>
+            </div>
+            <div class="options news">
+                <a href="#">
+                    LARABITS
+                </a>
+                <span>
+                    new!
+                </span>
+            </div>
+            <div class="options">
+                <a href="#">
+                    DISCUSSIONS
+                </a>
+            </div>
+            <div class="options">
+                <a href="#">
+                    PODCAST
+                </a>
+            </div>
+        </div>
+        <!-- zona dedicata all'utente -->
+        <div class="right_header d-flex">
+            <i class="fas fa-search"></i>
+            <span>SING IN</span>
+            <button>GET STARTED</button>
+        </div>
+    </section>
 </header>
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,6 @@ const mix = require('laravel-mix');
  */
 
  mix.js('resources/js/app.js', 'public/js')
- .copyDirectory('resources/img', 'public/img')
  .sass('resources/sass/app.scss', 'public/css')
  .options({
      processCssUrls: false
